ad7d8df0e8c456fd44bc452c5520e9af1c69f513
Jacob <Jacob.SH@icloud.com>
2021-02-09 01:03:20 +0100

    Free Cash Flow added

--- main.py
+++ main.py
@@ -26,26 +26,80 @@ json_data = json.loads(script_data[start:-12])
 
 json_data['context']['dispatcher']['stores']['QuoteSummaryStore'].keys()
 
-annual_is = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['incomeStatementHistory']['incomeStatementHistory']
+##print(json_data['context']['dispatcher']['stores']['QuoteSummaryStore'].keys())
 
-print(annual_is)
+annual_incomeStatement = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['incomeStatementHistory']['incomeStatementHistory']
+annual_cashflowStatement = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements']
+annual_blanceSheet = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['balanceSheetHistory']['balanceSheetStatements']
 
-annual_cf_statement = []
+##print(annual_is)
 
-for s in annual_is:
+annual_cashflow_statement = []
+annual_incomeStatement_statement = []
+annual_blanceSheet_statement = []
+
+for s in annual_cashflowStatement:
     statement = {}
     for key, val in s.items():
         try:
             ##options are: raw, fmt, longFmt
-            statement[key] = val['longFmt']
+            statement[key] = val['raw']
         except KeyError:
             continue
         except TypeError:
             continue
-    annual_cf_statement.append(statement)
+    annual_cashflow_statement.append(statement)
 
-##print(annual_cf_statement[0])
+##print(annual_cashflow_statement[0])
 
-print("Ebit: " + str(annual_cf_statement[0]['ebit']))
-print("Net income: " + str(annual_cf_statement[0]['netIncome']))
-print("Gross Profit: " + str(annual_cf_statement[0]['grossProfit']))
+for s in annual_incomeStatement:
+    statement = {}
+    for key, val in s.items():
+        try:
+            ##options are: raw, fmt, longFmt
+            statement[key] = val['raw']
+        except KeyError:
+            continue
+        except TypeError:
+            continue
+    annual_incomeStatement_statement.append(statement)
+
+##print(annual_incomeStatement_statement[0])
+
+for s in annual_blanceSheet:
+    statement = {}
+    for key, val in s.items():
+        try:
+            ##options are: raw, fmt, longFmt
+            statement[key] = val['raw']
+        except KeyError:
+            continue
+        except TypeError:
+            continue
+    annual_blanceSheet_statement.append(statement)
+
+## FCF = Net income + intrest expense + 
+
+tax = 0.21
+capex = annual_cashflow_statement[0]['capitalExpenditures']
+depri = annual_cashflow_statement[0]['depreciation']
+ebit  = annual_incomeStatement_statement[0]['ebit']
+
+_delta_current_assets     = (annual_blanceSheet_statement[0]['totalCurrentAssets'] - (annual_blanceSheet_statement[1]['totalCurrentAssets']))
+_delta_current_liabilites = (annual_blanceSheet_statement[0]['totalCurrentLiabilities'] - (annual_blanceSheet_statement[1]['totalCurrentLiabilities']))
+change_in_current_asset   = ((_delta_current_assets) - (_delta_current_liabilites))
+
+print("_______________________________________________________________")
+print()
+print("!CURRENT STOCK: " + stock)
+print()
+print("Tax rate: " + str(tax) + " %")
+print("EBIT: " + str(ebit))
+print("Depreciation and Amortization: " + str(depri))
+print("CAPEX: " + str(capex))
+print("Change in (Current Assets - Current Liabilites): " + str(change_in_current_asset))
+print()
+unlevered_fcf = ((ebit * (1-tax)) + (depri) - (capex) - (change_in_current_asset))
+
+print("Free cashflow: " + str(unlevered_fcf))
+print()
